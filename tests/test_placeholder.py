22dfff698a0a5bbdb5800293514e04ea952339bf
Paul Tunison <paul.tunison@kitware.com>
2022-11-28 12:00:42 -0500

    Add a placeholder test, flake8 should ignore a `.venv`
    Initial CI pytest calls fail with a non-zero exit code if there are
    absolutely no tests. A placeholder test module and function are added to
    be at least something for pytest to see and run so that there is a
    success return.
    
    Also found flake8 in CI checking into the local virtual environment
    which is of course not desired, so add the configured directory to the
    exclude list.

---
+++ tests/test_placeholder.py
@@ -0,0 +1,7 @@
+def test_placeholder() -> None:
+    """
+    This is an intentionally empty test so that there is at least one thing to
+    be run during a test pass.
+
+    This test and file should be removed when there are real tests.
+    """
