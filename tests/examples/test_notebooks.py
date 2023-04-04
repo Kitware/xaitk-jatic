3ccc7a3b7d786cb062e95f94615fab3183139bc5
Emily Veenhuis <emily.veenhuis@kitware.com>
2023-04-04 19:20:42 +0000

    Image Classification Protocol Notebook

--- tests/examples/test_notebooks.py
+++ tests/examples/test_notebooks.py
@@ -7,7 +7,9 @@ from jatic_toolbox.testing.pyright import list_error_messages, pyright_analyze
 @pytest.mark.parametrize("filepath, expected_num_errors", [
     ("examples/huggingface/xaitk-huggingface.ipynb", 0),
     ("examples/lightning/xaitk-lightning.ipynb", 1),
-    ("examples/mlflow/MNIST_MLFlow_scikit_saliency.ipynb", 0)
+    ("examples/mlflow/MNIST_MLFlow_scikit_saliency.ipynb", 0),
+    ("examples/jatic_toolbox/jatic-object-detector-protocol.ipynb", 0),
+    ("examples/jatic_toolbox/jatic-image-classifier-protocol.ipynb", 0),
 ])
 def test_pyright_nb(filepath: str, expected_num_errors: int) -> None:
     results = pyright_analyze(filepath)
