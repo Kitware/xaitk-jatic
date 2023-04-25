f0673d6af504d0beafdc8a647a7318a6955693d5
Bharadwaj Ravichandran <barry.ravichandran@kitware.com>
2023-04-25 17:03:47 -0400

    Added default values and new state variables

--- examples/gradio/gr_component_state.py
+++ examples/gradio/gr_component_state.py
@@ -1,41 +1,59 @@
-# Choice of model
-model_name = []
+# Choice of image classification model
+img_cls_model_name = []
 
-# Choice of saliency algorithm
-saliency_algo_name = []
+# Choice of object detection model
+obj_det_model_name = []
+
+# Choice of image classification saliency algorithm
+img_cls_saliency_algo_name = []
+
+# Choice of object detection saliency algorithm
+obj_det_saliency_algo_name = []
 
 # Number of threads to utilize when generating masks
-threads_state = []
+threads_state = [4]
 
 # Window_size for SlidingWindowStack algorithm
-window_size_state = []
+window_size_state = ['(50,50)']
 
 # Stride for SlidingWindowStack algorithm
-stride_state = []
+stride_state = ['(20,20)']
 
-# Number of random masks for RISEStack algorithm
-num_masks_state = []
+# Number of random masks for RISEStack/DRISEStack algorithm
+num_masks_state = ['200']
 
-# Spatial resolution of masking grid for RISEStack algorithm
-spatial_res_state = []
+# Spatial resolution of masking grid for RISEStack/DRISEStack algorithm
+spatial_res_state = ['8']
 
 # Probability of the grid cell being set to 1 (otherwise 0)
-p1_state = []
+p1_state = [0.5]
 
 # Random seed to allow for reproducibility
-seed_state = []
+seed_state = [0]
 
-# Debiased option for RISEStack saliency algorithm
-debiased_state = []
+# Debiased option for RISEStack/DRISEStack saliency algorithm
+debiased_state = [False]
+
+# Occlusion grid cell size in pixels for RandomGridStack algorithm
+occlusion_grid_state = ['(128,128)']
 
 
-def select_model(model_choice):
-    model_name.append(model_choice)
+def select_img_cls_model(model_choice):
+    img_cls_model_name.append(model_choice)
+    return model_choice
+
+def select_obj_det_model(model_choice):
+    obj_det_model_name.append(model_choice)
     return model_choice
 
 
-def select_saliency_algo(sal_choice):
-    saliency_algo_name.append(sal_choice)
+def select_img_cls_saliency_algo(sal_choice):
+    img_cls_saliency_algo_name.append(sal_choice)
+    return sal_choice
+
+
+def select_obj_det_saliency_algo(sal_choice):
+    obj_det_saliency_algo_name.append(sal_choice)
     return sal_choice
 
 
@@ -77,3 +95,8 @@ def enter_seed(seed):
 def check_debiased(debiased):
     debiased_state.append(debiased)
     return debiased
+
+
+def enter_occlusion_grid_size(val):
+    occlusion_grid_state.append(val)
+    return val
